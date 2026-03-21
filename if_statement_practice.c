8cd7e44336b6642995029729d3147511aa60cbb6
thechef <115754739+szakacspeter88@users.noreply.github.com>
2026-03-21 01:28:15 +0100

    Update if_statement_practice.c

--- if_statement_practice.c
+++ if_statement_practice.c
@@ -5,16 +5,16 @@ int main() {
   int age;
 
   printf("\nEnter your age: ");
-  scanf("%d",) &age;
+  scanf("%d", &age);
 
-  if(age >= 0){
-    printf("You are signed up!");
+  if(age >= 18){
+    printf("You are signed up!\n");
   }
   else if(age < 0){
-      printf("You haven't been born yet!"); 
+      printf("You haven't been born yet!\n"); 
   }
   else {
-      printf("You are too joung to sign up!");  
+      printf("You are too joung to sign up!\n");  
   }
   
   return 0;
